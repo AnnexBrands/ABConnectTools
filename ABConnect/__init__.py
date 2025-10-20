26060a894c43e5a857463446e95b9c580d0768f5
Brett Monson <bmonson@annexbrands.com>
2025-10-20 12:22:36 -0600

    ship choices includes list with account, price

--- ABConnect/__init__.py
+++ ABConnect/__init__.py
@@ -3,7 +3,13 @@ from .Builder import APIRequestBuilder
 from .Quoter import Quoter
 from .api import ABConnectAPI
 
-__all__ = ["FileLoader", "APIRequestBuilder", "Quoter", "ABConnectAPI"]
+# Create models alias for convenient imports
+# Usage: from ABConnect.models import ChangeJobAgentRequest
+import sys
+from .api import models
+sys.modules['ABConnect.models'] = models
+
+__all__ = ["FileLoader", "APIRequestBuilder", "Quoter", "ABConnectAPI", "models"]
 
 __version__ = "0.2.0"
 VERSION = "0.2.0"
